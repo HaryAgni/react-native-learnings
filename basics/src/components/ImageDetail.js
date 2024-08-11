bdb4f38560032508d6559b9d0c3cdb9e1a0a19ef
Harshit Agnihotri <haryagni123@gmail.com>
2024-08-12 00:04:48 +0530

    added components

---
+++ basics/src/components/ImageDetail.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text } from "react-native";
+
+const ImageDetail = () => {
+  return <Text>Image Detail</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
---
+++ basics/src/screens/ImageScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import ImageDetail from "../components/ImageDetail";
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetail/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;
