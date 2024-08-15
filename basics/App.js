1d8bc21b633e00848df07c680317aa2b46030ac5
Harshit Agnihotri <haryagni123@gmail.com>
2024-08-15 21:20:53 +0530

    understanding positioning in react native

--- basics/App.js
+++ basics/App.js
@@ -1,6 +1,5 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ListScreen from "./src/screens/ListScreen";
--- basics/src/screens/BoxScreen.js
+++ basics/src/screens/BoxScreen.js
@@ -3,11 +3,38 @@ import { StyleSheet, Text, View } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <View style={styles.viewOneStyle}></View>
+      <View style={styles.viewTwoStyle}></View>
+      <View style={styles.viewThreeStyle}></View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    backgroundColor:'gray',
+    height: 150,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "orange",
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "white",
+    marginTop: 50,
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green",
+  },
+});
 export default BoxScreen;
