8bed3df03c3f8d50608b700b91d27e4cb6044c62
Harshit Agnihotri <haryagni123@gmail.com>
2024-08-29 23:59:14 +0530

    added bottom tab navigation

--- navigation-types/App.js
+++ navigation-types/App.js
@@ -1,47 +1,40 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 import { Ionicons } from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      <BottomTab.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: "purple" },
-          headerTintColor: "white",
-          sceneContainerStyle: { backgroundColor: "orange" },
-          drawerInactiveTintColor: "blue",
-          drawerActiveTintColor: "orange",
-          drawerItemStyle: {
-            borderWidth: 2,
-            borderRadius: 8,
-            borderColor: "pink",
-          },
+          headerTintColor:'white',
+          tabBarActiveTintColor:'green'
         }}
       >
-        <Drawer.Screen
+        <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="home" color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name="User"
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="person" color={color} size={size} />
             ),
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
