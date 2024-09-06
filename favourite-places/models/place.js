0e0fef769902658fba0805946078c969f2e452ad
Harshit Agnihotri <haryagni123@gmail.com>
2024-09-06 17:26:18 +0530

    favourite places project setup

---
+++ favourite-places/models/place.js
@@ -0,0 +1,9 @@
+class places {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location;
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}
